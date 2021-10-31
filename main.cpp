e4829a02282994fa8c7204788a45916835966398
tienchaunmt <tienchaunmt@gmail.com>
2021-10-31 15:16:05 +0700

    Replace cls with clrscr

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <Windows.h>
 
 using std::cout;
 using std::endl;
 
+void clrscr()
+{
+	CONSOLE_SCREEN_BUFFER_INFO	csbiInfo;                  
+	HANDLE	hConsoleOut;
+	COORD	Home = {0,0};
+	DWORD	dummy;
+
+	hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	GetConsoleScreenBufferInfo(hConsoleOut,&csbiInfo);
+
+	FillConsoleOutputCharacter(hConsoleOut,' ',csbiInfo.dwSize.X * csbiInfo.dwSize.Y,Home,&dummy);
+	csbiInfo.dwCursorPosition.X = 0;
+	csbiInfo.dwCursorPosition.Y = 0;
+	SetConsoleCursorPosition(hConsoleOut,csbiInfo.dwCursorPosition);
+}
+
 int main(int argc, char** argv) {
-  system("cls");
+  clrscr();
   cout << "Hello World!" << endl;
   system("pause");
   return 0;
